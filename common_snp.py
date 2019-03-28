54545d1b38eb5d22c968da922e965c568f90235f
cdbope <christianbope@aims.ac.za>
2019-03-28 13:32:45 +0200

    Create common_snp.py

---
+++ common_snp.py
@@ -0,0 +1,22 @@
+import os
+from sets import *
+
+import gzip
+
+if len(sys.argv) < 2:
+    print "Usage: %s inputfile_1 ... <inputfile_n> outputfile"%sys.argv[0]
+    exit()
+
+INFILENAMES = sys.argv[1:-1]
+OUTFILENAME = sys.argv[-1]
+
+file1 = open(sys.argv[1])
+print "Reading %s"%file1.name
+for line in file1:
+        if not line.strip(): continue
+        data = [s.strip() for s in line.split()] #remove space
+        if data[-1] in rows:
+                rows[data[-1]].append(data[:-1])
+        else:
+                rows[data[-1]]=[data[:-1]]
+file1.close()
